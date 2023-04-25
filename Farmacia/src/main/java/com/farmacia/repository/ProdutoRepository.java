e4d4171be84bf5c4ea33f5b319024e5e9ca6c698
Daiane Bittencourt <daibittencourt2000@gmail.com>
2023-04-25 15:06:16 -0300

    CRUD com relação entre tabelas

---
+++ Farmacia/src/main/java/com/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,17 @@
+package com.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.farmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository <Produto, Long > { // foi adicionado a HERANÇA com a
+	// interface JpaRepository que recebe dois parametros -> Classe categoria e a chave primária Long (id)
+	
+	public List <Produto> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo); // criando Query Method 
+	// vai fazer uma consulta personalizada | método assinado com uma Collection List 
+	
+
+}
